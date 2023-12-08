7182c70b9e6e61b003a1d665b638f32ead31b582
wncfe <alekseip2002@gmail.com>
2023-12-08 17:55:49 +0300

    nc

---
+++ app/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, Query, Depends
+from typing import Optional
+from datetime import date
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+class HotelSearchArgs():
+    def __init__(
+        self,
+        location: str,
+        date_in: date,
+        date_out: date,
+        has_spa: Optional[bool] = None,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+    ):
+        self.location = location
+        self.date_in = date_in
+        self.date_out = date_out
+        self.has_spa = has_spa
+        self.stars = stars
+
+@app.get('/hotels')
+def get_hotels(
+    search_args: HotelSearchArgs = Depends()
+):
+    hotels = [
+        {
+            'adress': 'Улица Макарова, 2, Москва',
+            'name': 'Super Star',
+            'stars': 5,
+        },
+    ]
+    return search_args
+
+
+class SBooking(BaseModel):
+    hotel_id: int
+    date_in: date
+    date_out: date
+
+@app.post('/bookings')
+def add_booking(booking: SBooking):
+    pass
+    
